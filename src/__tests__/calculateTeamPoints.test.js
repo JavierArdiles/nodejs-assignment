8e4159e73bc4061486cb6cc8e7251393daefa0ff
JavierArdiles <javierardilesreibel@gmail.com>
2021-11-26 10:20:40 -0300

    missing README

--- src/__tests__/calculateTeamPoints.test.js
+++ src/__tests__/calculateTeamPoints.test.js
@@ -1,2 +1,20 @@
-describe('calculateTeamPoints', () => {
+const calculateTeamPoints = require('../calculateTeamPoints');
+const players = require('./__data__/players.json');
+describe('calculateTeamPoints(players)', () => {
+    it('Should return: [{team: "green", points: 40},{team: "red", points: 20},{team: "blue", points: 10}]', function(){
+        expect(calculateTeamPoints(players)).toEqual([
+            {
+                team: 'green',
+                points: 40
+            },
+            {
+                team: 'red',
+                points: 20
+            },
+            {
+                team: 'blue',
+                points: 10
+            }
+        ])
+    })
 })
--- src/calculateTeamPoints.js
+++ src/calculateTeamPoints.js
@@ -3,6 +3,38 @@
 // The output is sorted in descending order by points
 // input: [{ team: 'green', name: 'Bob', points: 5, isActive: true }, ...]
 // output: [{ team: 'green', points: 40 }, ...]
+
 module.exports = (players) => {
-  return []
+  let arr = [];
+  let red = 0;
+  let green = 0;
+  let blue = 0;
+  for(let i = 0; i < players.length; i++){
+    if(players[i].isActive){
+      if(players[i].team === 'red') red += players[i].points;
+      else if(players[i].team === 'green') green += players[i].points;
+      else if(players[i].team === 'blue') blue += players[i].points;
+    }
+  }
+  let teamRed = {
+    team: 'red',
+    points: red
+  };
+  let teamGreen = {
+    team: 'green',
+    points: green
+  };
+  let teamBlue = {
+    team: 'blue',
+    points: blue
+  };
+  arr.push(teamRed);
+  arr.push(teamGreen);
+  arr.push(teamBlue);
+  let output = arr.sort((a,b) => {
+    if(a.points > b.points) return -1;
+    if(a.points < b.points) return 1;
+    return 1;
+  });
+  return output;
 }
