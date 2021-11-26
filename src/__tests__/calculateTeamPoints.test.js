9d81131bd1f5a196a97f425a993315a854bb6105
JavierArdiles <javierardilesreibel@gmail.com>
2021-11-26 11:14:40 -0300

    finished

--- src/__tests__/calculateTeamPoints.test.js
+++ src/__tests__/calculateTeamPoints.test.js
@@ -1,7 +1,7 @@
 const calculateTeamPoints = require('../calculateTeamPoints');
 const players = require('./__data__/players.json');
 describe('calculateTeamPoints(players)', () => {
-    it('Should return sorted array', function(){
+    it('Should return: [{team: "green", points: 40},{team: "red", points: 20},{team: "blue", points: 10}]', function(){
         expect(calculateTeamPoints(players)).toEqual([
             {
                 team: 'green',
